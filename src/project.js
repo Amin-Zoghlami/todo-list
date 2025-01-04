62abcfa3c54b565c92e2f21a86d254b62cec77e0
Amin Zoghlami <amin.zoghlami.361@my.csun.edu>
2025-01-04 15:01:17 -0800

    Refactor todo, project, and app

--- src/project.js
+++ src/project.js
@@ -1,62 +1,16 @@
 export default class Project {
-    #title;
-    #todos = [];
+    title;
+    todos = [];
     
     constructor (title) {
-        this.#title = title;
+        this.title = title;
     }
     
-    setTitle(title) {
-        this.#title = title;
-    }
-
-    getTitle() {
-        return this.#title;
-    }
-
-    getTodos() {
-        return [...this.#todos];
-    }
-    
-    getTodo(index) {
-        return this.getTodos()[index];
-    }
-
-    getTodoTitle(index) {
-        return this.getTodo(index).getTitle();
-    }
-
-    getTodoDescription(index) {
-        return this.getTodo(index).getDescription();
-    }
-
-    getTodoDueDate(index) {
-        return this.getTodo(index).getDueDate();
-    }
-
-    getTodoPriority(index) {
-        return this.getTodo(index).getPriority();
-    }
-    
-    size() {
-        return this.getTodos().length;
-    }
-    
-    setTodoProperties(title, description, dueDate, priority, index) {
-        const todo = this.#todos[index];
-        todo.setTitle(title);
-        todo.setDescription(description);
-        todo.setDueDate(dueDate);
-        todo.setPriority(priority);
-    }
-
     addTodo(todo) {
-        this.#todos.push(todo);
+        this.todos.push(todo);
     }
 
     removeTodo(index) {
-        this.#todos.splice(index, 1);
+        this.todos.splice(index, 1);
     }
-
-    
 };
--- src/todo.js
+++ src/todo.js
@@ -1,55 +1,9 @@
 export default class Todo {
-    #title;
-    #description
-    #dueDate;
-    #priority;
-    #isComplete;
-
     constructor (title, description, dueDate, priority) {
-        this.#title = title;
-        this.#description = description;
-        this.#dueDate = dueDate;
-        this.#priority = priority;
-        this.#isComplete = false;
-    }
-
-    setTitle(title) {
-        this.#title = title;
-    }
-
-    getTitle() {
-        return this.#title;
-    }
-
-    setDescription(description) {
-        this.#description = description;
-    }
-    
-    getDescription() {
-        return this.#description;
-    }
-
-    setDueDate(dueDate) {
-        this.#dueDate = dueDate;
-    }
-    
-    getDueDate() {
-        return this.#dueDate;
-    }
-
-    setPriority(priority) {
-        this.#priority = priority;
-    }
-    
-    getPriority() {
-        return this.#priority;
-    }
-
-    getIsComplete() {
-        return this.#isComplete;
-    }
-
-    switchIsComplete() {
-        this.#isComplete = !this.#isComplete;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.isComplete = false;
     }
 };
